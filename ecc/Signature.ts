335fee8804cf0810349f6bafa1f61d903151daef
lilli2203 <mailyourlilli@gmail.com>
2024-07-27 10:45:30 +0530

    made changes in signarute

--- ecc/Signature.ts
+++ ecc/Signature.ts
@@ -1,10 +1,14 @@
 import { bigToBuf, bigFromBuf } from "../util/BigIntUtil";
 import * as bufutil from "../util/BufferUtil";
+import { ec as EC } from "elliptic";
+import { createHash } from "crypto";
+
+const ec = new EC("secp256k1");
 
 export class Signature {
   constructor(readonly r: bigint, readonly s: bigint) {}
 
-  public static parse(buffer: Buffer) {
+  public static parse(buffer: Buffer): Signature {
     let pos = 0;
 
     const prefix = buffer.readUInt8(pos);
@@ -18,15 +22,11 @@ export class Signature {
       throw new Error("Bad signature length");
     }
     pos += 1;
-
-    // verify marker byte
     let marker = buffer.readUInt8(pos);
     if (marker !== 0x02) {
       throw new Error("Bad signature");
     }
     pos += 1;
-
-    // read r-length
     const rlen = buffer.readUInt8(pos);
     pos += 1;
 
@@ -48,11 +48,11 @@ export class Signature {
     return new Signature(r, s);
   }
 
-  public toString() {
+  public toString(): string {
     return `Signature_${this.r}_${this.s}`;
   }
 
-  public der() {
+  public der(): Buffer {
     const encodePart = (v: bigint) => {
       let bytes = bigToBuf(v);
 
@@ -70,4 +70,55 @@ export class Signature {
 
     return Buffer.concat([Buffer.from([0x30, r.length + s.length]), r, s]);
   }
+
+  public verify(message: string | Buffer, publicKey: string | Buffer): boolean {
+    const msgHash = Signature.hashMessage(message);
+    const key = ec.keyFromPublic(publicKey, "hex");
+    return key.verify(msgHash, { r: bigToBuf(this.r), s: bigToBuf(this.s) });
+  }
+
+  public static sign(message: string | Buffer, privateKey: string): Signature {
+    const msgHash = Signature.hashMessage(message);
+    const key = ec.keyFromPrivate(privateKey, "hex");
+    const sig = key.sign(msgHash);
+    return new Signature(bigFromBuf(sig.r.toArrayLike(Buffer)), bigFromBuf(sig.s.toArrayLike(Buffer)));
+  }
+
+  public static hashMessage(message: string | Buffer): Buffer {
+    return createHash("sha256").update(message).digest();
+  }
+
+  public toHex(): string {
+    return this.der().toString("hex");
+  }
+
+  public toBuffer(): Buffer {
+    return this.der();
+  }
+
+  public static fromHex(hex: string): Signature {
+    return Signature.parse(Buffer.from(hex, "hex"));
+  }
+  public split(): { r: Buffer; s: Buffer } {
+    return {
+      r: bigToBuf(this.r),
+      s: bigToBuf(this.s),
+    };
+  }
+  public static combine(r: Buffer, s: Buffer): Signature {
+    return new Signature(bigFromBuf(r), bigFromBuf(s));
+  }
 }
+const privateKey = ec.genKeyPair().getPrivate("hex");
+const message = "Hello, blockchain!";
+const signature = Signature.sign(message, privateKey);
+console.log(`Signature: ${signature.toHex()}`);
+const publicKey = ec.keyFromPrivate(privateKey).getPublic("hex");
+const isValid = signature.verify(message, publicKey);
+console.log(`Signature is valid: ${isValid}`);
+const signatureBuffer = signature.toBuffer();
+const parsedSignature = Signature.parse(signatureBuffer);
+console.log(`Parsed Signature: ${parsedSignature.toHex()}`);
+const { r, s } = signature.split();
+const combinedSignature = Signature.combine(r, s);
+console.log(`Combined Signature: ${combinedSignature.toHex()}`);
