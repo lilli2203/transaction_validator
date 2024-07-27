7764e018ea52e5bd90ebc53a6228f466b7069516
lilli2203 <mailyourlilli@gmail.com>
2024-07-27 11:16:40 +0530

    Delete util directory

--- util/Hash256.ts
+++
@@ -1,8 +0,0 @@
-import crypto from "crypto";
-
-export function hash256(buf: Buffer): Buffer {
-  return crypto
-    .createHash("sha256")
-    .update(crypto.createHash("sha256").update(buf).digest())
-    .digest();
-}
--- util/Sha256.ts
+++
@@ -1,5 +0,0 @@
-import crypto from "crypto";
-
-export function sha256(buf: Buffer): Buffer {
-  return crypto.createHash("sha256").update(buf).digest();
-}
